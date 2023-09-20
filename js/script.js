d71e97b865d5f945004f63de8cbe9fdb79e5d9c1
Berodtm <134790573+Berodtm@users.noreply.github.com>
2023-09-20 21:37:17 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// script.js
+// document.addEventListener('DOMContentLoaded', () => {
+//     const articles = document.querySelectorAll('article');
+//     articles.forEach((article, index) => {
+//       article.addEventListener('click', () => {
+//         alert(`You clicked on Week ${index + 1}`);
+//       });
+//     });
+//   });
+  
